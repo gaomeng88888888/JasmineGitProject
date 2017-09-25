793e09bed93e622cee3887a083fc471413fc64e8
gaomeng <meng.gao@altenchina.com>
2017-09-25 14:14:14 +0800

    新增：数据类型转换package

---
+++ jasmineGitProject/src/com/jasmine/typeConversion/TypeConversion.java
@@ -0,0 +1,24 @@
+package com.jasmine.typeConversion;
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author gaomeng
+ * @date Sep 25, 2017 1:48:40 PM 
+ * @version V1.0
+ */
+public class TypeConversion {
+	
+	/*
+	 * 编译器报错：java.lang.Error: Unresolved compilation problem:Type mismatch: cannot convert from int to short
+	 * 当short+1的时候返回值转换为int，再次赋值给s的时候发生short to int类型转换失败
+	 */
+	@Test
+	public void shortToInt(){
+		short s = 1;
+//		s = s + 1;
+	}
+	
+	
+}
