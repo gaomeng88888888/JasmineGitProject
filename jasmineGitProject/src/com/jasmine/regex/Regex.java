81403a9565c9c0f3d16729d6940b2431e5c8fb9d
gaomeng <meng.gao@altenchina.com>
2017-09-25 10:31:12 +0800

    正则表达式

---
+++ jasmineGitProject/src/com/jasmine/regex/Regex.java
@@ -0,0 +1,31 @@
+package com.jasmine.regex;
+
+import java.util.regex.Pattern;
+
+public class Regex {
+	public static void main(String[] args) {
+		System.out.println(regex("", ""));
+	}
+	
+	/**
+	 * 
+	 * @author jasmine
+	 */
+	public static boolean regex(String regex, String str){
+		return Pattern.compile(regex).matcher(str).matches();
+	}
+	/*
+	public static void regexx(){
+		// 要验证的字符串
+	    String str = "jwj is jwisda";
+	    // 验证规则
+	    String regEx = "^.*a$";
+	    // 编译正则表达式
+	    Pattern pattern = Pattern.compile(regEx);
+	    Matcher matcher = pattern.matcher(str);
+	    // 字符串是否与正则表达式相匹配
+	    boolean rs = matcher.matches();
+	    System.out.println(rs);
+	}
+	*/
+}
